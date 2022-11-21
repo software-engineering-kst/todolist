3b526c675641fd5d660e7424be9921b9823c5f4f
Kuanysh <kuanysh4646@gmail.com>
2022-11-21 21:49:36 +0600

    Revert "1"
    This reverts commit 64649f8535918ebf74b6e6f8e29689c5d938c294.

--- src/main/java/kz/lakida/todolist/repository/TaskRepository.java
+++ src/main/java/kz/lakida/todolist/repository/TaskRepository.java
@@ -1,11 +1,5 @@
 package kz.lakida.todolist.repository;
 
-import kz.lakida.todolist.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.UUID;
-@Repository
-public interface TaskRepository extends JpaRepository<Task, UUID>
+public interface TaskRepository //extends JpaRepository<Task, UUID>
 {
 }
--- src/main/java/kz/lakida/todolist/web/GetTasksResponse.java
+++ src/main/java/kz/lakida/todolist/web/GetTasksResponse.java
@@ -3,28 +3,9 @@ package kz.lakida.todolist.web;
 import kz.lakida.todolist.model.Task;
 import lombok.Data;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class GetTasksResponse {
-
-  private List<Task> taskList;
-
-    public GetTasksResponse() {
-    }
-
-    public GetTasksResponse(List<Task> taskList) {
-        this.taskList = taskList;
-    }
-
-    public List<Task> getTaskList() {
-        return taskList;
-    }
-    public GetTasksResponse(Iterable<Task> items) {
-        items.forEach(taskList:: add);
-    }
-    public void setTaskList(List<Task> taskList) {
-        this.taskList = taskList;
-    }
+    private final List<Task> tasks;
 }
