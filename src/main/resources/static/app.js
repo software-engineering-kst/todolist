7c18212f46642b9924ca98e4dc64af964ca9a0ff
Anton Lakida <antonlakida@gmail.com>
2022-11-21 23:16:21 +0600

    Flyway migration

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,3 +1,18 @@
+function createUUID() {
+    // http://www.ietf.org/rfc/rfc4122.txt
+    var s = [];
+    var hexDigits = "0123456789abcdef";
+    for (var i = 0; i < 36; i++) {
+        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    }
+    s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+    s[8] = s[13] = s[18] = s[23] = "-";
+
+    var uuid = s.join("");
+    return uuid;
+}
+
 const App = {
     data() {
         return { 
@@ -14,7 +29,17 @@ const App = {
         },
         addNewNote(){
             if (this.inputValue !== ''){
-                this.notes.push(this.inputValue)
+                fetch("http://localhost:8080/tasks",
+                {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({id: createUUID(), name: this.inputValue})
+                })
+                  .then((response) => {
+                    console.log(response);
+                  });
                 this.inputValue =''
             }
             
