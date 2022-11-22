7755c42f8119d20bf541fc8c8116590ec7e37ccd
Kuanysh <kuanysh4646@gmail.com>
2022-11-22 16:49:21 +0600

    some commit

--- src/main/java/kz/lakida/todolist/repository/TaskRepository.java
+++ src/main/java/kz/lakida/todolist/repository/TaskRepository.java
@@ -1,5 +1,10 @@
 package kz.lakida.todolist.repository;
 
+import kz.lakida.todolist.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
 public interface TaskRepository extends JpaRepository<Task, UUID>
 {
 }
